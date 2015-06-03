80536d1599e592575a57cc941f34979f247374d9
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-06-03 15:14:12 +0530

    Added the libraries symlinks added last time didn't include code

---
+++ quad_summer/libraries/PID_v1/PID_v1.h
@@ -0,0 +1,81 @@
+#ifndef PID_v1_h
+#define PID_v1_h
+#define LIBRARY_VERSION	1.1.1
+
+class PID
+{
+
+
+  public:
+
+  //Constants used in some of the functions below
+  #define AUTOMATIC	1
+  #define MANUAL	0
+  #define DIRECT  0
+  #define REVERSE  1
+
+  //commonly used functions **************************************************************************
+    PID(int*, int*, int*,        // * constructor.  links the PID to the Input, Output, and 
+        int, int, int, int);     //   Setpoint.  Initial tuning parameters are also set here
+	
+    void SetMode(int Mode);               // * sets PID to either Manual (0) or Auto (non-0)
+
+    bool Compute();                       // * performs the PID calculation.  it should be
+                                          //   called every time loop() cycles. ON/OFF and
+                                          //   calculation frequency can be set using SetMode
+                                          //   SetSampleTime respectively
+
+    int SetOutputLimits(int, int); //clamps the output to a specific range. 0-255 by default, but
+										  //it's likely the user will want to change this depending on
+										  //the application
+	
+    int SetILimits(int, int);
+
+
+  //available but not commonly used functions ********************************************************
+    void SetTunings(int, int,       // * While most users will set the tunings once in the 
+                    int);         	  //   constructor, this function gives the user the option
+                                          //   of changing tunings during runtime for Adaptive control
+	void SetControllerDirection(int);	  // * Sets the Direction, or "Action" of the controller. DIRECT
+										  //   means the output will increase when error is positive. REVERSE
+										  //   means the opposite.  it's very unlikely that this will be needed
+										  //   once it is set in the constructor.
+    void SetSampleTime(int);              // * sets the frequency, in Milliseconds, with which 
+                                          //   the PID calculation is performed.  default is 100
+										  
+										  
+										  
+  //Display functions ****************************************************************
+	int GetKp();						  // These functions query the pid for interal values.
+	int GetKi();						  //  they were created mainly for the pid front-end,
+	int GetKd();						  // where it's important to know what is actually 
+	int GetMode();						  //  inside the PID.
+	int GetDirection();					  //
+
+  private:
+	void Initialize();
+	
+	int dispKp;				// * we'll hold on to the tuning parameters in user-entered 
+	int dispKi;				//   format for display purposes
+	int dispKd;				//
+    
+	int kp;                  // * (P)roportional Tuning Parameter
+    int ki;                  // * (I)ntegral Tuning Parameter
+    int kd;                  // * (D)erivative Tuning Parameter
+
+	int controllerDirection;
+
+    int *myInput;              // * Pointers to the Input, Output, and Setpoint variables
+    int *myOutput;             //   This creates a hard link between the variables and the 
+    int *mySetpoint;           //   PID, freeing the user from having to constantly tell us
+                                  //   what these values are.  with pointers we'll just know.
+			  
+	unsigned long lastTime;
+	int ITerm, lastInput;
+
+	unsigned long SampleTime;
+	int outMin, outMax,I_outMax,I_outMin;
+	bool inAuto;
+};
+#endif
+
