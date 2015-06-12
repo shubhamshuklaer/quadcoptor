e3b555336453230fffd15b8b2642b1382ced818f
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-06-12 20:02:38 +0530

    Basic show history working

--- quad_summer/Quad-app/app/src/main/java/com/quad/shubham/quad_app/Show_history.java
+++ quad_summer/Quad-app/app/src/main/java/com/quad/shubham/quad_app/Show_history.java
@@ -3,10 +3,13 @@ package com.quad.shubham.quad_app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 //import android.view.ViewGroup.LayoutParams;
 
@@ -33,14 +36,33 @@ public class Show_history extends Activity {
         select_branch.setSelection(((Branch_list_cursor_adapter) (select_branch.getAdapter())).getPosition(branch_name));
 
 
+
+
         commits_list =new ListView(Show_history.this);
         commits_list.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, Gravity.FILL));
 
 
-        commits_list.setAdapter(new Commit_list_cursor_adapter(Show_history.this,db_helper.get_commits_for_branch(branch_name)));
+        commits_list.setAdapter(new Commit_list_cursor_adapter(Show_history.this, db_helper.get_commits_for_branch(branch_name)));
 
         layout.addView(select_branch);
         layout.addView(commits_list);
+
+        select_branch.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView t_v = (TextView) view;
+                String branch_name = t_v.getText().toString();
+                Db_helper db_helper = new Db_helper(Show_history.this);
+                commits_list.setAdapter(new Commit_list_cursor_adapter(Show_history.this, db_helper.get_commits_for_branch(branch_name)));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                commits_list.setAdapter(null);
+            }
+        });
+
+
         setContentView(layout);
     }
 }
