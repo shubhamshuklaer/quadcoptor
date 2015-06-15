10f38b9576c028ef3e2739d63cbdb2f990dbe07f
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-06-15 19:01:41 +0530

    Socket closed error removed but still the service crashes once in a while

--- quad_summer/Quad-app/app/src/main/java/com/quad/shubham/quad_app/Data_logging_service.java
+++ quad_summer/Quad-app/app/src/main/java/com/quad/shubham/quad_app/Data_logging_service.java
@@ -34,6 +34,7 @@ public class Data_logging_service extends IntentService{
     final byte new_line_ascii='\n';
     final byte carrige_return_ascii='\r';
     public static final String intent_filter_prefix="com.quad.shubham.quad_app";
+    volatile boolean stop;
 
 
     private BroadcastReceiver data_receiver=new BroadcastReceiver() {
@@ -53,9 +54,10 @@ public class Data_logging_service extends IntentService{
     @Override
     protected void onHandleIntent(Intent intent) {
         if(initialize(intent)){
-            while(!Thread.currentThread().interrupted()){
+            while(!stop){
                 receive();
             }
+            destroy();
         }
     }
 
@@ -71,6 +73,7 @@ public class Data_logging_service extends IntentService{
             o_stream=socket.getOutputStream();
             data=new byte[buffer_size];
             buffer_pos=0;
+            stop=false;
             return true;
         }catch (IOException e){
             Log.e("normal", e.getMessage());
@@ -95,38 +98,47 @@ public class Data_logging_service extends IntentService{
     private void receive() {
         byte read_byte;
         try {
-            while(i_stream.available() >0 && buffer_pos < buffer_size) {
-                read_byte =(byte) (i_stream.read());//reads 1 byte... since returns a int between 0-255 we can cast it directly
-                if (read_byte == carrige_return_ascii) {
-                    String data_line=new String(data,0,buffer_pos,"US-ASCII");
-                    data_line=data_line.trim().replaceAll("\\s+", " ");//Will trim and convert all multiple spaces to single
-                    if(data_line.charAt(data_line.length()-1)=='\n'){
-                        data_line=data_line.substring(0,data_line.length()-1);//removing the last /n
+            if( buffer_pos >= buffer_size) {
+                stop=true;
+                Log.e("normal","Buffer overflow");
+            }else if (i_stream.available() >0) {
+                int read_byte_int=i_stream.read();
+                if(read_byte_int!=-1) {
+                    read_byte = (byte) (read_byte_int);//reads 1 byte... since returns a int between 0-255 we can cast it directly
+                    if (read_byte == carrige_return_ascii) {
+                        String data_line = new String(data, 0, buffer_pos, "US-ASCII");
+                        data_line = data_line.trim().replaceAll("\\s+", " ");//Will trim and convert all multiple spaces to single
+                        if (data_line.charAt(data_line.length() - 1) == '\n') {
+                            data_line = data_line.substring(0, data_line.length() - 1);//removing the last /n
+                        }
+                        String[] seperated = data_line.split(" ", 2);// Data line will be of format "prefix int int\n"
+                        if (seperated.length == 2) { //Sometimes it is lenght 1 causes error
+                            Intent intent = new Intent(Data_logging_service.intent_filter_prefix + ":" + seperated[0]);
+                            intent.putExtra("data", seperated[1]);
+                            LocalBroadcastManager.getInstance(Data_logging_service.this.getApplicationContext()).sendBroadcast(intent);
+                        }
+                        buffer_pos = 0;
+                    } else {
+                        data[buffer_pos] = read_byte;
+                        buffer_pos++;
                     }
-                    String[] seperated=data_line.split(" ",2);// Data line will be of format "prefix int int\n"
-                    if(seperated.length==2) { //Sometimes it is lenght 1 causes error
-                        Intent intent = new Intent(Data_logging_service.intent_filter_prefix + ":" + seperated[0]);
-                        intent.putExtra("data", seperated[1]);
-                        LocalBroadcastManager.getInstance(Data_logging_service.this.getApplicationContext()).sendBroadcast(intent);
-                    }
-                    buffer_pos=0;
-                } else if (read_byte==-1){
-                    Log.d("normal","End of stream reached");
-                    stopSelf();
-                }else{
-                    data[buffer_pos]=read_byte;
-                    buffer_pos++;
+                }else {
+                    Log.d("normal", "End of stream reached");
+                    stop = true;
                 }
             }
         }catch(IOException e){
-            Log.e("normal", e.getMessage());
-            stopSelf();
+            Log.e("normal",e.getMessage());
+            stop=true;
         }
     }
 
     @Override
     public void onDestroy() {
-        destroy();
+        stop=true;
+        //from https://github.com/android/platform_frameworks_base/blob/master/core/java/android/app/IntentService.java
+        //super.onDestroy() only quits the looper i.e no more requests will be taken and also all pending
+        //requests are cleared... but the current request will still run
         super.onDestroy();
     }
 }
