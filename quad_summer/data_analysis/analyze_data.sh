b8a0e18699b18c42e568dc1faed78da786d67c94
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-07-07 17:23:25 +0530

    Now merging all graphs into 1 pdf

--- quad_summer/data_analysis/analyze_data.sh
+++ quad_summer/data_analysis/analyze_data.sh
@@ -3,6 +3,7 @@
 if [ $# -ne 1 ]
 then
     echo "usage ./analyze_data.sh log_file_name"
+    exit 2
 fi
 
 echo "Splitting data"
@@ -12,7 +13,26 @@ mkdir temp
 
 echo "Processing data"
 mkdir -p output
-matlab -nodesktop -nosplash -nodisplay -r "run('process_data.m');exit;"
+#using basename to get the filename from path
+prefix=$(basename $1)"_"
+matlab -nodesktop -nosplash -nodisplay -r "process_data('"$prefix"');exit;"
+rm -rf temp
+
+for file_name in output/*; do
+    #file_name will be of forrmat output/something.something
+    base_file_name=$(basename $file_name) #will get something.something
+    if [[ $base_file_name =~ ^$prefix.* ]]
+    then
+        merge_cmd=$merge_cmd" "$file_name
+    fi
+done
+
+if [ "$merge_cmd" != "" ]
+then
+    echo "Merging graphs"
+    merge_cmd="pdfunite $merge_cmd output/$prefix""merged.pdf"
+    eval $merge_cmd
+fi
 
 echo "output graphs are stored in output dir you can use pdf viewer to open"
-rm -rf temp
+echo "file prefix is "$prefix
--- quad_summer/data_analysis/process_data.m
+++ quad_summer/data_analysis/process_data.m
@@ -1,19 +1,21 @@
-d=dir('temp');
+function process_data(prefix)
+    d=dir('temp');
 
-for n=1:numel(d)
-    file_name=fullfile('temp',d(n).name);
-    fid=fopen(file_name);
-    if fid~=-1
-        data=textscan(fid,'%f,%f');
-        figure('Visible','off');
-        plot(data{1},data{2});
-        title(d(n).name);
-        print('-depsc2','-r300',fullfile('output',d(n).name));
-        fclose(fid);
-    else
-        if ~isdir(file_name)
-            fprintf('cannot open file %s\n',d(n).name);
+    for n=1:numel(d)
+        file_name=fullfile('temp',d(n).name);
+        fid=fopen(file_name);
+        if fid~=-1
+            data=textscan(fid,'%f,%f');
+            figure('Visible','off');
+            plot(data{1},data{2});
+            title(d(n).name);
+            print('-dpdf','-r300',fullfile('output',strcat(prefix,d(n).name,'.pdf')));
+            fclose(fid);
+        else
+            if ~isdir(file_name)
+                fprintf('cannot open file %s\n',d(n).name);
+            end
         end
-    end
 
+    end
 end
