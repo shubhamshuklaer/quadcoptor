ec707a9320d443180b63831e7d89b14f7bde57cf
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-07-07 16:25:49 +0530

    Figure title working

--- quad_summer/data_analysis/analyze_data.sh
+++ quad_summer/data_analysis/analyze_data.sh
@@ -11,8 +11,8 @@ mkdir temp
 ./split_data.awk $1
 
 echo "Processing data"
-#no graphs will be displayed because of -nodisplay
 mkdir -p output
 matlab -nodesktop -nosplash -nodisplay -r "run('process_data.m');exit;"
+
 echo "output graphs are stored in output dir you can use pdf viewer to open"
 rm -rf temp
--- quad_summer/data_analysis/process_data.m
+++ quad_summer/data_analysis/process_data.m
@@ -5,8 +5,9 @@ for n=1:numel(d)
     fid=fopen(file_name);
     if fid~=-1
         data=textscan(fid,'%f,%f');
-        f=figure('Visible','off');
+        figure('Visible','off');
         plot(data{1},data{2});
+        title(d(n).name);
         print('-depsc2','-r300',fullfile('output',d(n).name));
         fclose(fid);
     else
