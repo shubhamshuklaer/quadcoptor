b7e0c275ecc5cc6ee3260b328fcc4a243d294490
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-07-10 17:37:10 +0530

    Now data_analysis draws graphs according to the config file

--- quad_summer/data_analysis/process_data.m
+++ quad_summer/data_analysis/process_data.m
@@ -1,21 +1,30 @@
 function process_data(prefix)
-    d=dir('temp');
-
-    for n=1:numel(d)
-        file_name=fullfile('temp',d(n).name);
-        fid=fopen(file_name);
-        if fid~=-1
-            data=textscan(fid,'%f,%f');
+    config_fid=fopen('config');
+    if config_fid~=-1
+        while ~feof(config_fid)
+            config_line=strtrim(fgets(config_fid));
+            series = regexp(config_line, ',', 'split');
             figure('Visible','off');
-            plot(data{1},data{2});
-            title(d(n).name,'interpreter','none');
-            print('-dpdf','-r300',fullfile('temp',strcat(prefix,d(n).name,'.pdf')));
-            fclose(fid);
-        else
-            if ~isdir(file_name)
-                fprintf('cannot open file %s\n',d(n).name);
+            color_map=hsv(numel(series)); % creating a HSC colormap
+            hold on
+            for k=1:numel(series)
+                file_name=fullfile('temp',series{k})
+                fid=fopen(file_name);
+                if fid~=-1
+                    data=textscan(fid,'%f,%f');
+                    plot(data{1},data{2},'color',color_map(k,:));
+                    fclose(fid);
+                else
+                    fprintf('cannot open file %s\n',series{k});
+                end
             end
+            legend(series,'interpreter','none'); %otherwise it interprets string as latex and '_' gives subscript
+            hold off
+            title(config_line,'interpreter','none');
+            print('-dpdf','-r300',fullfile('temp',strcat(prefix,config_line,'.pdf')));
         end
-
+        fclose(config_fid);
+    else
+        fprintf('cannot open file "config"');
     end
 end
